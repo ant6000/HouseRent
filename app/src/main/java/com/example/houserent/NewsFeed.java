ae3d97b0fca2b67c5eeea8cdfff88a4e0fe366d2
antor <antorofficial20@gmail.com>
2023-10-31 16:31:41 +0600

    project reuploaded

---
+++ app/src/main/java/com/example/houserent/NewsFeed.java
@@ -0,0 +1,15 @@
+package com.example.houserent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class NewsFeed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_feed);
+
+    }
+}
